872528a28adb4611e99818bb2b902559fa2c1acc
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-19 02:21:00 +0900

    Title: 체크 다이아몬드, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240719/체크 다이아몬드/check-diamond.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int k;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) { //줄 개수
+        for (k = 0; k < n - i - 1; k++) {//공백 2--> 1 --> 0
+            cout << " ";
+        } 
+         cout << "*"; 
+        
+        for (int j = 0; j < i ; j++) { //공백별 0,1,2
+            cout << " " << "*";
+        }
+            cout << endl;
+        }
+    for (int i = 0; i < n - 1; i++) { //줄개수
+        if (i == n - 2) {
+            cout << " ";
+        }
+        for (int j = 0; j < (n-1)-i ; j++) { //2-->1
+            cout << " " << "*";
+        }
+       
+        cout << endl;
+    }
+    
+        return 0;
+    
+}
