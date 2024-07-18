aa8af652567a33a3af497f692dd703aa7c676102
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-18 23:04:10 +0900

    Title: 홀수만 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240718/홀수만 출력하기/print-only-odd-numbers.cpp
+++ 240718/홀수만 출력하기/print-only-odd-numbers.cpp
@@ -8,7 +8,7 @@ int main() {
     int n2;
     for ( int i = 0; i < n; i++) {
         cin >> n2;
-        if(n2 % 3 == 0) {
+        if((n2 % 3 == 0) && n2 % 2 == 1) {
             cout << n2 << endl;
         }
     }
