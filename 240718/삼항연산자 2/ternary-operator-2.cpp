55b00cc6b2ec35a46b661895c29ab80e34ded011
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-18 21:25:55 +0900

    Title: 삼항연산자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240718/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if (a == 1) {
+        cout << "t" << endl;
+    } else {
+        cout << "f" << endl;
+    }
+    return 0;
+}
