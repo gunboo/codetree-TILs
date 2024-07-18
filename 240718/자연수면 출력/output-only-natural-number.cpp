b1025530e8b1c66f7b4c61f479ae59ce70bb4fdc
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-18 22:57:21 +0900

    Title: 자연수면 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240718/자연수면 출력/output-only-natural-number.cpp
+++ 240718/자연수면 출력/output-only-natural-number.cpp
@@ -4,9 +4,11 @@ using namespace std;
 int main() {
     int a, b;
     cin >> a >> b;
-
+    
+    if (a > 0) {
     for (int i = 0; i < b; i++ ) {
         cout << a;
     }
+    } else cout << "0" << endl;
     return 0;
 }
