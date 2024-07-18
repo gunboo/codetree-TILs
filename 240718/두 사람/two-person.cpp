f425b729fb272b87fe4c3d829c021f1ca6847c77
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-18 21:49:11 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240718/두 사람/two-person.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age1,age2;
+    char sex1, sex2;
+
+    cin >> age1 >> sex1;
+    cin >> age2 >> sex2;
+
+    if(age1 >= 19 || age2 >= 19) {
+        if(sex1 == 'M' || sex2 == 'M')
+        cout << "1" << endl;
+    } else cout << "0" << endl;
+    return 0;
+}
