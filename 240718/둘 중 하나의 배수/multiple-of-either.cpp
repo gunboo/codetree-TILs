a3b1e20ebd76addcaad6d70a9d50316fa58196bc
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-18 21:44:00 +0900

    Title: 둘 중 하나의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240718/둘 중 하나의 배수/multiple-of-either.cpp
+++ 240718/둘 중 하나의 배수/multiple-of-either.cpp
@@ -5,8 +5,9 @@ int main() {
     int a;
     cin >> a;
 
-    if(a % 3 == 0 && a % 5 == 0) {
+    if(a % 3 == 0 || a % 5 == 0) {
         cout << "1" << endl;
     } else cout << "0" << endl;
+
     return 0;
 }
