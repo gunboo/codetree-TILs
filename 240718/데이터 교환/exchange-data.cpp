4cf0e6068c18a7c9a4be63c4924f48057dc57a5c
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-18 20:51:03 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240718/데이터 교환/exchange-data.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 6, c = 7;
+    b = 5;
+    c = 6;
+    a = 7;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+   return 0;
+}
