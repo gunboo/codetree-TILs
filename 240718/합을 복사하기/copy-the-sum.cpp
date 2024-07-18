9369b6e8a0d1cf17fb13187f53997094a70518c3
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-18 20:52:14 +0900

    Title: 합을 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240718/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 1, b = 2, c = 3;
+    a = 6;
+    b = 6;
+    c = 6;
+    cout << a << " " << b << " " << c << endl;
+    return 0;
+}
