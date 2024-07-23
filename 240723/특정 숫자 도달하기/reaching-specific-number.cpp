f774e490933b7780ffc75924732abcf042c47930
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-23 16:08:51 +0900

    Title: 특정 숫자 도달하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240723/특정 숫자 도달하기/reaching-specific-number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4] >> arr[5] >> arr[6] >>  arr[7] >>arr[8] >> arr[9];
+
+    cout << fixed;
+    cout.precision(1);
+
+    int sum = 0;
+    double avg = 0;
+    int num = 0;
+
+    for(int i = 0; i < 9; i++) {
+        if (arr[i] >= 250) break;
+        sum += arr[i];
+        num++;
+    }   
+    avg = (double)sum / (double)num;
+    cout << sum << " " << avg << endl;
+    return 0;
+}
