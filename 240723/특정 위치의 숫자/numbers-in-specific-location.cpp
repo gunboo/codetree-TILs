56acd8395358ca59ff8634e9a82ad416a4f5082d
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-23 21:34:19 +0900

    Title: 특정 위치의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/특정 위치의 숫자/numbers-in-specific-location.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    for ( int i = 0; i < 10; i++) {
+        cin >> arr[i];
+    }
+    int sum = arr[2] + arr[4] + arr[9];
+     cout << sum << endl;
+
+    return 0;
+}
