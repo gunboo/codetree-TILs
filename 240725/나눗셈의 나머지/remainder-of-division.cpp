a4701f54a822edcd41dd7ff90a7d5cb74feffc00
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-25 01:28:10 +0900

    Title: 나눗셈의 나머지, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240725/나눗셈의 나머지/remainder-of-division.cpp
+++ 240725/나눗셈의 나머지/remainder-of-division.cpp
@@ -5,14 +5,11 @@ int main() {
     int a, b;
     cin >> a >> b; //a, b입력받고
     int count_num[11] = {0,}; //나머지를 셀 숫자
-    int rest[11] = {0,};
     int sum = 0;
-    int i = 0;
+    
    while(a > 1) {
-        rest[i] = a % b; //rest에 나머지 저장
-        count_num[rest[i]]++;
+        count_num[a % b]++;
         a /= b; //몫 a에 다시 저장
-        i++;
     }
     for (int i = 0; i < 11; i++) {
          sum += (count_num[i] * count_num[i]);
