c512ad0656a3ca28e296f174b6acfd4a044c0ab7
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-27 00:48:07 +0900

    Title: 문자열 역순으로 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240727/문자열 역순으로 출력하기/print-string-in-reverse.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str[4];
+    for(int i = 0; i < 4; i++) {
+        cin >> str[i];
+    }
+        cout << str[i] << endl;
+    \}
+   
+    
+    return 0;
+\}
+$0
+    for(int i = 3; i >= 0; i--) {
+        cout << str[i] << endl;
+    }
+   
+    
+    return 0;
+}
