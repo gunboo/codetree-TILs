dde1da2a668bdf9f52bfec88fb484d72d7478559
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-27 00:48:16 +0900

    Title: 문자열 역순으로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240727/문자열 역순으로 출력하기/print-string-in-reverse.cpp
+++ 240727/문자열 역순으로 출력하기/print-string-in-reverse.cpp
@@ -7,13 +7,6 @@ int main() {
     for(int i = 0; i < 4; i++) {
         cin >> str[i];
     }
-        cout << str[i] << endl;
-    \}
-   
-    
-    return 0;
-\}
-$0
     for(int i = 3; i >= 0; i--) {
         cout << str[i] << endl;
     }
