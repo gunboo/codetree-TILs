9b962411530654f63720ba8c82a7a89146b615a4
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-27 00:28:52 +0900

    Title: 문자열의 특정 위치 찾기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240727/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.cpp
+++ 240727/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.cpp
@@ -18,9 +18,3 @@ int main() {
     cout << num;
     return 0;
 }
-for(int i = 0; i < 5; i++) {
-    if(arr[i][2] == a || arr[i][3] == a) {
-        cout << arr[i] << endl;
-        num++;
-    }
-}
