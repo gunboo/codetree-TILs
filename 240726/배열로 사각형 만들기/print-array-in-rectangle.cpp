9dbd66b4b1224be76e96faa761dc6b61556f95ce
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-26 17:37:56 +0900

    Title: 배열로 사각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240726/배열로 사각형 만들기/print-array-in-rectangle.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5][5];
+    int num = 1;
+    for(int i = 0; i < 5; i++) { //모든 배열 1로 초기화
+        for(int j = 0; j < 5; j++){
+            arr[i][j] = 1;
+        }
+    }
+    for(int i = 1; i < 5; i++) {
+        for(int j = 1; j < 5; j++) {
+        arr[i][j] = arr[i-1][j] + arr[i][j-1];
+    }
+    }
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5; j++) {
+            cout << arr[i][j] << " "; 
+        }
+        cout << endl;
+    }
+    
+     return 0;
+}
