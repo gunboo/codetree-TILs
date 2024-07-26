50b946c49b677ded6afd85ef6e00759e3428acc7
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-27 02:29:42 +0900

    Title: 문자열 범위 출력하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240727/문자열 범위 출력하기 2/print-string-in-range-2.cpp
+++ 240727/문자열 범위 출력하기 2/print-string-in-range-2.cpp
@@ -8,15 +8,16 @@ int main() {
     int num;
     cin >> num;
     int len = str.length();
-    if(num > len) {
-        for(int i = len-1; i > 0; i--) {
-            cout << str[i];
-        }
-    }
-    else  {
+    if (num <= len) {
         for(int i = len-1; i > len-1-num; i--) {
             cout << str[i];
     }
     }
+    else {
+        for(int i = len-1; i >= 0; i--) {
+            cout << str[i];
+        }
+    }
+
     return 0;
 }
