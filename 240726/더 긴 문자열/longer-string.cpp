5af9bbdd61e2b31a2c1ac85c9d95f7c8ba8b4c75
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-26 20:45:38 +0900

    Title: 더 긴 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240726/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str1, str2;
+    cin >> str1 >> str2;
+    if(str1.length() == str2.length()) {
+        cout << "same" << endl;
+    }
+    else if(str1.length() > str2.length()){
+        cout << str1 << " " << str1.length() << endl;
+    }
+    else {
+        cout << str2 << " " << str2.length() << endl;
+    }
+    return 0;
+}
