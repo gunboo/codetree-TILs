00a0322d782a3d2ba772caf6216cdeae87c91f1f
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-27 01:20:45 +0900

    Title: 단어로 구분하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240727/단어로 구분하기 3/separate-words-with-words-3.cpp
+++ 240727/단어로 구분하기 3/separate-words-with-words-3.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     string str[10];
     for(int i = 0; i < 10; i++) {
-        cin >> str[10];
+        cin >> str[i];
     }
     for(int i = 9; i >= 0; i--) {
         cout << str[i] << endl;
