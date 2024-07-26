fd571bc240ecce8db486a55a5fb9378f3623b0bf
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-27 02:14:39 +0900

    Title: 문자열에서 문자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240727/문자열에서 문자 출력/print-chars-from-word.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string arr;
+    cin >> arr;
+    int len = arr.length();
+    for(int i = 0; i < len; i++) {
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
