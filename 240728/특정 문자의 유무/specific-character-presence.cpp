6ffda0e276a8a3df0e31340634d29909dc6be19e
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-28 21:40:25 +0900

    Title: 특정 문자의 유무, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240728/특정 문자의 유무/specific-character-presence.cpp
+++ 240728/특정 문자의 유무/specific-character-presence.cpp
@@ -5,25 +5,15 @@ using namespace std;
 int main() {
     string str;
     cin >> str;
-    int len = str.length();
-
-    bool found_ee = false;
-    bool found_ab = false;
-
-    for(int i = 0; i < len - 1; i++) {
-        if(str[i] == 'e' && str[i+1] == 'e') {
-           found_ee = true;
-        }
-        if(str[i] == 'a' && str[i+1] == 'b') {
-            found_ab = true;
-        }
-    }
-    if(found_ee) {
-        cout << "Yes" << " ";
-    }else cout <<"No" << " ";
-    if(found_ab) {
-        cout << "Yes";
-    }else cout << "No";
     
+    if(str.find("ee") != string::npos) 
+        cout << "Yes ";
+    else 
+        cout << "No ";
+
+    if(str.find("ab") != string::npos) 
+        cout << "Yes";
+    else    
+        cout << "No";
     return 0;
 }
