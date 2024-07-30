ec95dcdcf476fc2a145bd9f5aeca19fc5766abbf
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-30 15:18:07 +0900

    Title: 아스키코드의 합과 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240730/아스키코드의 합과 차/sum-and-subtract-in-ASCII.cpp
+++ 240730/아스키코드의 합과 차/sum-and-subtract-in-ASCII.cpp
@@ -5,6 +5,12 @@ int main() {
     char a, b;
     cin >> a >> b;
 
-    cout << (int)a + (int)b << " " << (int)b - (int)a;
+    cout << (int)a + (int)b << " " ;
+    if(a > b) {
+        cout << (int)a - (int)b;
+    }
+    else {
+        cout << (int)b - (int)a;
+    }
     return 0;
 }
