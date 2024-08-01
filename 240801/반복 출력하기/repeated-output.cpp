57c359ec2bb12c44d1ffa9a58c21bb6099e9328b
gunboo <150986713+gunboo@users.noreply.github.com>
2024-08-01 21:23:15 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240801/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void print(int a) 
+{
+    for(int i = 0; i < a; i++) {
+        cout << "12345^&*()_" << endl;
+    }
+}
+int main() {
+    int n;
+    cin >> n;
+
+    print(n);
+    return 0;
+}
