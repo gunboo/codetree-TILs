ee412f6a11fbc2c0873d7bd166bb7e9f64b5ce70
gunboo <150986713+gunboo@users.noreply.github.com>
2024-08-05 03:24:19 +0900

    Title: 정수의 최솟값, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240805/정수의 최솟값/minimum-value-of-an-integer.cpp
+++ 240805/정수의 최솟값/minimum-value-of-an-integer.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 int sum (int x, int y, int z) {
-    if(x > y && y > z) return z;
-    else if(y > z && z > x) return x;
-    else if(x > z && z > y) return y;
+    if(x >= y && y > z) return z;
+    else if(y >= z && z > x) return x;
+    else if(x >= z && z > y) return y;
 }
 int main() {
     int a, b, c;
