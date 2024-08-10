44c98dba56564a25b2eeb9832e81fe2ca83d01a5
gunboo <150986713+gunboo@users.noreply.github.com>
2024-08-11 02:17:21 +0900

    Title: 함수를 이용한 윤년 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240811/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
+++ 240811/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 bool year(int y) {
-    if(y % 4 == 0) return true;
-    if(y % 100 == 0) return false;
-    if(y % 400 == 0) return true;
-    return false;
+    if (y % 400 == 0) return true; //400으로 나누어 떨어지면 윤년
+    if (y % 100 == 0) return false; //100으로 떨어지면 평년
+    if ( y % 4 == 0 ) return true;
+    else return false;
 }
 int main() {
     int n;
